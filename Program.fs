6ab129949be8cecef0270abb6a85826c3970a96b
cannorin <cannorin@gmail.com>
2016-01-22 13:12:37 +0900

    Use Suave instead

--- Program.fs
+++ Program.fs
@@ -1,21 +1,8 @@
 ﻿module twiblocker.Main
-
 open System
-open Nancy
-open Nancy.Hosting
-
-type DemoApp () =
-    inherit NancyModule ()
-    do
-        let Get = base.Get
-        Get.["/"] <- fun parameters -> "Hello from F#/Nancy on Heroku!" :> obj
+open Suave
 
 [<EntryPoint>]
-let main args = 
-    let env_port = Environment.GetEnvironmentVariable("PORT")
-    let port = if env_port = null then "1234" else env_port
-
-    let nancy_host = new Nancy.Hosting.Self.NancyHost(new Uri("http://localhost:" + port))
-    nancy_host.Start()
-    while true do Console.ReadLine() |> ignore
+let main argv =
+    startWebServer defaultConfig (Successful.OK "Hello World!")
     0
